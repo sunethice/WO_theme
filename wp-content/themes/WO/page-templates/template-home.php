47fc46e5b42fa2a6de1304d1910b0ce658a4abd7
Suneth Perera <sunethjoes@gmail.com>
2020-08-19 15:19:03 +1000

    commit changes

--- wp-content/themes/WO/page-templates/template-home.php
+++ wp-content/themes/WO/page-templates/template-home.php
@@ -18,6 +18,18 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<div class="row">
 		<div class="home-call-to-action col"></div>
 		<div class="home-company-marketing col">
+			<div class="magento-intro">
+				<h1>Magento Development</h1>
+				<p class="intro-breifing">
+					You will work with some of the best magento developers in Melbourne when working with 
+					us as our devlopers are seasoned and experiencedwith all versions of Magento including 
+					1.xand 2.x. Our Magento deployments runmulti-million dollar business and are driven by 
+					measurable results and continuous, sustainable growth over time.
+				</p>
+			</div>
+			<div class="">
+				<h2></h2>
+			</div>
 			<?php
 				get_footer();
 			?>
